a52942db252cc4114a03780a7feba8a8bb42ab92
intaniger <tanakornp@cleversecorp.com>
2023-10-16 17:05:48 +0700

    fix: :ambulance: fixed req.query type

--- src/exercise/handlers/query.ts
+++ src/exercise/handlers/query.ts
@@ -1,19 +1,39 @@
-import { RequestHandler } from "express";
+import { RequestHandler } from 'express'
 
 interface CalculationQuery {}
 
-export const PlusQueryHandler: RequestHandler<any, number, CalculationQuery> = (req, res) => {
+export const PlusQueryHandler: RequestHandler<
+  any,
+  number,
+  any,
+  CalculationQuery
+> = (req, res) => {
   res.send('TODO: plusQuery')
 }
 
-export const MinusQueryHandler: RequestHandler<any, number, CalculationQuery> = (req, res) => {
+export const MinusQueryHandler: RequestHandler<
+  any,
+  number,
+  any,
+  CalculationQuery
+> = (req, res) => {
   res.send('TODO: minusQuery')
 }
 
-export const MultiplyQueryHandler: RequestHandler<any, number, CalculationQuery> = (req, res) => {
+export const MultiplyQueryHandler: RequestHandler<
+  any,
+  number,
+  any,
+  CalculationQuery
+> = (req, res) => {
   res.send('TODO: multiplyQuery')
 }
 
-export const DivideQueryHandler: RequestHandler<any, number, CalculationQuery> = (req, res) => {
+export const DivideQueryHandler: RequestHandler<
+  any,
+  number,
+  any,
+  CalculationQuery
+> = (req, res) => {
   res.send('TODO: divideQuery')
 }
